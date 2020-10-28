1fe6d0ae27a54cb95a1e5724d13f7f51e1708e8f
DeekshithSystemsValley <deekshith@systemsvalley.co.in>
2020-10-28 17:17:23 +0530

    T0455-Modify advertising invoicing - updated features w.r.t properties

--- publishing_invoicing/models/sale_order.py
+++ publishing_invoicing/models/sale_order.py
@@ -4,10 +4,14 @@ from odoo import api, fields, models, _
 class SaleOrder(models.Model):
 	_inherit = 'sale.order'
 
-	invoicing_property_id = fields.Many2one('invoicing.property',string="Invoicing Property")
+	invoicing_property_id = fields.Many2one('invoicing.property',string="Invoicing Property",required=True)
+	invoicing_date = fields.Date(string="Invoicing Date")
+	inv_date_bool = fields.Boolean(string="Set attribute to Invoicing date field")
+	terms_condition = fields.Text(string="Terms and condition")
+	terms_cond_bool = fields.Boolean(string="Set attribute to Terms & condition field")
 
 	@api.multi
-	@api.onchange('partner_id')
+	@api.onchange('partner_id','advertising_agency')
 	def onchange_customer_publishing_invoicing(self):
 		for line in self:
 			if line.advertising_agency:
@@ -16,15 +20,62 @@ class SaleOrder(models.Model):
 			else:
 				if line.partner_id.invoicing_property_id:
 					line.invoicing_property_id = line.partner_id.invoicing_property_id.id
+	
 
-
+					
 	@api.multi
-	@api.onchange('advertising_agency')
-	def onchange_agency_publishing_invoicing(self):
+	@api.onchange('partner_id','advertising_agency')
+	def onchange_partner_package(self):
 		for line in self:
 			if line.advertising_agency:
 				if line.advertising_agency.invoicing_property_id:
-					line.invoicing_property_id = line.advertising_agency.invoicing_property_id.id
+					if line.invoicing_property_id.inv_package_deal == True:
+						line.package = True
+					else:
+						line.package = False
+			else:
+				if line.partner_id.invoicing_property_id:
+					if line.invoicing_property_id.inv_package_deal == True:
+						line.package = True
+					else:
+						line.package = False
+	@api.multi
+	@api.onchange('partner_id','advertising_agency')
+	def onchange_partner_invoicing_date(self):
+		for line in self:
+			if line.advertising_agency:
+				if line.advertising_agency.invoicing_property_id:
+					if line.invoicing_property_id.inv_whole_order_at_once == True:
+						line.inv_date_bool = True
+					else:
+						line.inv_date_bool = False
+			else:
+				if line.partner_id.invoicing_property_id:
+					if line.invoicing_property_id.inv_whole_order_at_once == True:
+						line.inv_date_bool = True
+					else:
+						line.inv_date_bool = False
+
+	@api.multi
+	@api.onchange('partner_id','advertising_agency')
+	def onchange_partner_pay_terms(self):
+		for line in self:
+			if line.advertising_agency:
+				if line.advertising_agency.invoicing_property_id:
+					if line.invoicing_property_id.pay_in_terms == True:
+						line.terms_cond_bool = True
+					else:
+						line.terms_cond_bool = False
+			else:
+				if line.partner_id.invoicing_property_id:
+					if line.invoicing_property_id.pay_in_terms == True:
+						line.terms_cond_bool = True
+					else:
+						line.terms_cond_bool = False
+
+
+
+
 
 
 class SaleOrderLine(models.Model):
