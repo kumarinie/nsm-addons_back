6d2b39d0f20cf151321f7758c9f60d66272e1b11
DeekshithSystemsValley <deekshith@systemsvalley.co.in>
2020-11-18 13:31:58 +0530

    Reference of a refund invoice line is added to sale_order_line.invoice_lines while creating a refund invoice

--- publishing_refunds_sales_integration/models/account_invoice.py
+++ publishing_refunds_sales_integration/models/account_invoice.py
@@ -10,24 +10,3 @@ class AccountInvoice(models.Model):
 	modify_refund_created = fields.Boolean(string="Modified refund created using this invoice")
 
 
-	@api.multi
-	def action_invoice_open(self):
-		res = super(AccountInvoice, self).action_invoice_open()
-		# Below field is used to avoid the multiple invoice id to sale_order_line.invoice_line
-		for inv in self:
-			for line in inv.invoice_line_ids:
-				sale_line_id = self.env['sale.order.line'].search([('id','=',line.so_line_id.id)])
-
-				if sale_line_id:
-					for sale_line in sale_line_id:
-						if inv.type =='out_invoice':
-							sale_line.invoice_lines = [(4, line.id)]
-						if inv.type == 'out_refund':
-							sale_line.invoice_lines = [(4, line.id)]
-					#fetch the invoice lines to add the removed lines from standard 
-						inv_line_ids = self.env['account.invoice.line'].search([('so_line_id','=',sale_line.id)])
-						for inv_line in inv_line_ids:
-							if inv_line.invoice_id.state != 'cancel':
-								sale_line.invoice_lines = [(4, inv_line.id)]
-		return res
-
