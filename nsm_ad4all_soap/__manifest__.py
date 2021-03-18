13b00ffadd90a32cfb0d56b2c223d6e5f625dfa2
Willem Hulshof <w.hulshof@magnus.nl>
2021-03-18 21:26:01 +0100

    Revert "depends publishing_invoicing"
    This reverts commit 0bff567e0be33ecbfce3df7475abcd07ffd9bd89.

--- nsm_ad4all_soap/__manifest__.py
+++ nsm_ad4all_soap/__manifest__.py
@@ -116,7 +116,7 @@ To contribute to this module, please visit https://odoo-community.org.
 
     # any module necessary for this one to work correctly
     'depends': [
-                'publishing_invoicing',
+                'sale_advertising_order',
                 'sale_advertising_operating_unit'
     ],
     'external_dependencies': {
