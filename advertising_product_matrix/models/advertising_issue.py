18c1983509d330165a0952cbfa303953ed822842
kumarinie <kuamrisushmanie@gmail.com>
2023-03-07 13:37:02 +0530

    Delete matrix module

--- advertising_product_matrix/models/advertising_issue.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class AdvertisingIssue(models.Model):
-    _inherit = 'sale.advertising.issue'
-
-    adv_pro_type_ids = fields.Many2many('advertising.product.matrix', string='Advertising Product Types')
