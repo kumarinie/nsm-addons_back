8d617b2304667978d88af2b6629135211724de32
kumarinie <kuamrisushmanie@gmail.com>
2023-08-11 17:38:19 +0530

    Enhance partner search with ref and zip

--- nsm_sale_advertising_order/models/res_partner.py
+++ nsm_sale_advertising_order/models/res_partner.py
@@ -20,3 +20,56 @@ class ResPartner(models.Model):
                 name = '%s' % (partner.name)
             res.append((partner.id, name))
         return res
+
+    def name_get_custom_ref(self, partner_ids):
+        if not partner_ids:
+            return []
+        res = []
+        domain = False
+        if 'searchFor' in self.env.context:
+            domain = self.env.context['searchFor']
+        for record in partner_ids:
+            str_name = record.name
+            if domain:
+                if record.zip and domain == 'zip':
+                    name = '['+record.ref+']'+str_name+'['+record.zip+']'
+                    res.append((record.id, name))
+                elif record.ref and domain == 'ref':
+                    name = '['+record.ref+']'+str_name+'['+record.zip+']'
+                    res.append((record.id, name))
+                else:
+                    res.append((record.id, str_name))
+            else:
+                res.append((record.id, str_name))
+        return res
+
+    @api.model
+    def name_search(self, name, args=None, operator='ilike', limit=100):
+        if not args:
+            args = []
+        args = args[:]
+        ctx = self.env.context.copy()
+        ctx.update({'searchFor': 'name'}) #default search for name
+        if name:
+            partner_ids = self.search([('zip', '=like', name + "%")] + args, limit=limit)
+            ctx.update({'searchFor': 'zip'}) if partner_ids else ctx
+            if not partner_ids:
+                partner_ids = self.search([('ref', '=like', name + "%")] + args, limit=limit)
+                ctx.update({'searchFor': 'ref'}) if partner_ids else ctx
+            partner_ids += self.search([('name', operator, name)] + args, limit=limit)
+            if not partner_ids and len(name.split()) >= 2:
+                # Separating zip, email and name of partner for searching
+                operand1, operand2 = name.split(' ', 1)  # name can contain spaces e.g. OpenERP S.A.
+                partner_ids = self.search([('zip', operator, operand1), ('name', operator, operand2)] + args,
+                                  limit=limit)
+                ctx.update({'searchFor': 'zip'}) if partner_ids else ctx
+                if not partner_ids:
+                    partner_ids = self.search([('ref', operator, operand1), ('name', operator, operand2)] + args,
+                                              limit=limit)
+                    ctx.update({'searchFor': 'ref'}) if partner_ids else ctx
+            if partner_ids:
+                return self.with_context(ctx).name_get_custom_ref(list(set(partner_ids)))
+            # else:
+            #     return []
+
+        return super(ResPartner, self).name_search(name, args, operator=operator, limit=limit)
